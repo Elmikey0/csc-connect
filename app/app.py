cebfb9a7a0e374cb26edd227bb77313e438dc1aa
Kaosi Anikwe <anikwehenryasa@gmail.com>
2024-12-03 14:45:34 +0000

    Corrected a message error

--- app/app.py
+++ app/app.py
@@ -338,7 +338,7 @@ def check_birthdays():
                 ):  # user[5] is the birthday
                     celebrants.append(user)
                     birthday_count +=1 #update birthday variable          
-                    message = f"Wishing a joyful birthday to you, {user[0]}, our melody-making friend! Your beautiful voice and uplifting spirit bring so much joy to our rehearsals and performances. May this special day be filled with love, music, and all the things that bring smile to your face. Keep shining bright like the star you are!🌟 Enjoy every moment and have a melodious birthday!🎶\n🎈#HappyBirthday{user[0].replace(" ", "")}"
+                    message = (f"Wishing a joyful birthday to you, {user[0]}, our melody-making friend! Your beautiful voice and uplifting spirit bring so much joy to our rehearsals and performances. May this special day be filled with love, music, and all the things that bring smile to your face. Keep shining bright like the star you are!🌟 Enjoy every moment and have a melodious birthday!🎶\n🎈#HappyBirthday{user[0].replace(" ", "")}")
                     print(f"Sending birthday message to: {user[4]}")  # Debug statement
                     send_email_notification("Birthdays are special! 🎂", message, user[4])     
                     time.sleep(1)
