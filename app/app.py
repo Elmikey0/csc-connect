1500235e6236d2075d67051cee253920f707969d
Kaosi Anikwe <103599091+kaosi-anikwe@users.noreply.github.com>
2025-02-09 18:45:58 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -394,10 +394,7 @@ def check_birthdays():
                 ):  # user[5] is the birthday
                     celebrants.append(user)
                     birthday_count +=1 #update birthday variable
-                    if "Okechukwu" in user[0] or "Chinenye" in user [0] or "Francisca" in user[0]:
-                        message = (f"Wishing a joyful birthday to you, {user[0].strip()}, our melody-making friend!❤️️ \nYour beautiful voice and uplifting spirit bring so much joy to our rehearsals and performances. May this special day be filled with love, music, and all the things that bring smile to your face. Keep shining bright like the star you are!🌟 Enjoy every moment and have a melodious birthday!🎶\n🎈#HappyBirthday{user[0].replace(' ', '')}\n\n\nHappy Birthday Nenye🤍")
-                    else:
-                        message = (f"Wishing a joyful birthday to you, {user[0].strip()}, our melody-making friend!❤️️ \nYour beautiful voice and uplifting spirit bring so much joy to our rehearsals and performances. May this special day be filled with love, music, and all the things that bring smile to your face. Keep shining bright like the star you are!🌟 Enjoy every moment and have a melodious birthday!🎶\n🎈#HappyBirthday{user[0].replace(' ', '')}")
+                    message = (f"Wishing a joyful birthday to you, {user[0].strip()}, our melody-making friend!❤️️ \nYour beautiful voice and uplifting spirit bring so much joy to our rehearsals and performances. May this special day be filled with love, music, and all the things that bring smile to your face. Keep shining bright like the star you are!🌟 Enjoy every moment and have a melodious birthday!🎶\n🎈#HappyBirthday{user[0].replace(' ', '')}")
                     send_email_notification("Birthdays are special! 🎂", message, user[4])     
                     
             if birthday_count > 0:
